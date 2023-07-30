e5e296ca696c0dc628ba4e8ba342407a9da5a268
duy1009 <duy10092001@gmail.com>
2023-07-30 17:46:03 +0700

    edit url

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 WEIGHT_PATH = r"./yolov7-tiny.pt"
-STREAM_URL = "https://www.youtube.com/watch?v=Emm2h0i6yAY"
+STREAM_URL = "https://www.youtube.com/watch?v=1-iS7LArMPA"
 im_size = 640
-conf_thres = 0.3
+conf_thres = 0.2
 iou_thres = 0.5
 device = 'cpu'
 classes = None
